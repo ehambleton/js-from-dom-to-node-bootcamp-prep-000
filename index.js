99568d306f1aa955aa1f139a2295b4e783b6493c
Edward Hambleton <ehambleton@me.com>
2017-12-02 23:23:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,21 +6,20 @@ if (5>2) {
 if (2>5) {
   console.log("2 is greater than 5")
 }
-var dog = "cuddly"
- 
+var dog = "playful"
+
 if (dog === "hungry") {
   console.log("Refilling food bowl.")
- 
+
 } else if (dog === "thirsty") {
   console.log("Refilling water bowl.")
- 
+
 } else if (dog === "playful") {
   console.log("Playing tug-of-war.")
- 
+
 } else if (dog === "cuddly") {
   console.log("Snuggling.")
- 
-} else { 
+
+} else {
   console.log("Reading newspaper.")
 }
- 
